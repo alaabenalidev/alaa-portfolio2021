d3ed2cc3c1bb56da72ad77645407cdb2458dcc51
alaabadev <alaa.benali.dev@gmail.com>
2021-11-05 16:37:07 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+import "tailwindcss/tailwind.css"
+
+// "start": "react-scripts start",
+//     "build": "react-scripts build",
+//     "test": "react-scripts test",
+
+function App() {
+  return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo"/>
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+  );
+}
+
+export default App;
---
+++ src/layouts/layout--base.jsx
@@ -0,0 +1,9 @@
+function App() {
+    return (
+        <div>
+            <h1>aa</h1>
+        </div>
+    )
+}
+
+export default App
