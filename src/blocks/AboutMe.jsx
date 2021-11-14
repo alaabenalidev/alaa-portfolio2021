b32b3aea1ce4f64f7a35411c609a0e392d72fdb1
Alaa Eddine Ben Ali <alaabenali.dev@gmail.com>
2021-11-14 23:23:19 +0100

    add AboutMe.jsx content

--- src/blocks/AboutMe.jsx
+++ src/blocks/AboutMe.jsx
@@ -1,9 +1,33 @@
 const AboutMe = () => {
-return (
-    <section className="w-full h-screen my-auto py-4">
-        <h1>About Me Page</h1>
-    </section>
-)
+    return (
+        <section className="w-full h-full my-auto py-4">
+            <h1>About Me Page</h1>
+            <h3>My introduction</h3>
+            <div className="grid grid-cols-1 md:grid-cols-5 gap-4 mt-5 h-auto ">
+                <div className="flex justify-center md:justify-end items-center col-span-2">
+                    <img
+                        src={require("../asstes/imgs/profile_welcoming.svg").default}
+                        className="w-8/12"/>
+                </div>
+                <div className="flex grid gap-4 col-span-3">
+                    <div className="flex items-end">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                            laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+                            voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                            cupidatat
+                            non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </div>
+
+                    <div className="w-6/12 place-self-center self-start flex grid grid-cols-3 gap-3 items-start justify-center">
+                        <h1>test1</h1>
+                        <h1>test2</h1>
+                        <h1>test3</h1>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
 }
 
 export default AboutMe
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,61 +13,16 @@ import {
 const Header = () => {
     return (
         <header>
-            {/*<nav className="flex items-center justify-between flex-wrap bg-gray-200 p-6 lg:pr-48 lg:pl-48">*/}
-            {/*    <div className="flex items-center flex-shrink-0 text-white mr-6 cursor-pointer">*/}
-            {/*        <svg className="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54"*/}
-            {/*             xmlns="http://www.w3.org/2000/svg">*/}
-            {/*            <path*/}
-            {/*                d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/>*/}
-            {/*        </svg>*/}
-            {/*        <span className="font-semibold text-blue-700 text-xl tracking-tight">Alaa Eddine Ben Ali</span>*/}
-            {/*    </div>*/}
-            {/*    <div className="flex lg:hidden">*/}
-            {/*        <MoonIcon className="h-6 w-6 text-gray-500 hover:text-blue-700 cursor-pointer m-2"/>*/}
-            {/*        <MenuAlt3Icon className="h-6 w-6 text-gray-500 hover:text-blue-700 cursor-pointer m-2"/>*/}
-            {/*    </div>*/}
-            {/*    <div className="w-full block flex-grow lg:items-center lg:flex lg:w-auto sm:hidden">*/}
-            {/*        <div className="text-sm lg:flex-grow">*/}
-            {/*            <a href="javascript:void(0);"*/}
-            {/*               className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">*/}
-            {/*                Docs*/}
-            {/*            </a>*/}
-            {/*            <a href="javascript:void(0);"*/}
-            {/*               className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">*/}
-            {/*                Examples*/}
-            {/*            </a>*/}
-            {/*            <a href="javascript:void(0);"*/}
-            {/*               className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">*/}
-            {/*                Blog*/}
-            {/*            </a>*/}
-            {/*        </div>*/}
-            {/*        <div className="flex">*/}
-            {/*            <a href="javascript:void(0);"*/}
-            {/*               className="block mt-4 lg:inline-block lg:mt-0 text-gray-500 hover:text-blue-700 mr-2 ml-2">*/}
-            {/*                Docs*/}
-            {/*            </a>*/}
-            {/*            <a href="javascript:void(0);"*/}
-            {/*               className="block mt-4 lg:inline-block lg:mt-0 text-gray-500 hover:text-blue-700 mr-2 ml-2">*/}
-            {/*                Examples*/}
-            {/*            </a>*/}
-            {/*            <a href="javascript:void(0);"*/}
-            {/*               className="block mt-4 lg:inline-block lg:mt-0 text-gray-500 hover:text-blue-700 mr-2 ml-2">*/}
-            {/*                Blog*/}
-            {/*            </a>*/}
-            {/*            <MoonIcon className="h-6 w-6 md:mt-4 text-gray-500 hover:text-blue-700 cursor-pointer"/>*/}
-            {/*        </div>*/}
-            {/*    </div>*/}
-            {/*</nav>*/}
             {/*Navbar goes here */}
-            <nav class="bg-gray-200 full shadow-sm fixed w-full">
+            <nav class="bg-gray-300 full shadow-sm fixed w-full z-10">
                 <div class="max-w-6xl mx-auto px-4">
                     <div class="flex justify-between">
                         <div class="flex space-x-7">
                             <div>
                                 {/*Website Logo */}
-                                <a href="#" class="flex items-center py-4 px-2">
-                                    <img src="logo.png" alt="Logo" class="h-8 w-8 mr-2"/>
-                                    <span class="font-semibold text-gray-500 text-lg tracking-widest">Navigation</span>
+                                <a href="#" class="flex items-center justify-center py-4 px-2">
+                                    {/*<img src="logo.png" alt="Logo" class="h-8 w-8 mr-2"/>*/}
+                                    <span class="font-semibold text-gray-500 text-lg tracking-widest">Alaa Eddine</span>
                                 </a>
                             </div>
                             {/*Primary Navbar items*/}
